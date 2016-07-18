aef1ebffa6733f0acfdd3644f79b78bbcaa33c19
ririyeye007 <ririyeye007@126.com>
2016-07-18 15:32:00 +0800

    Initial commit

---
+++ app/src/main/java/com/example/js/java_code/utilities/MyTestUtil.java
@@ -0,0 +1,10 @@
+package com.example.js.java_code.utilities;
+
+/**
+ * Created by Administrator on 2016/7/14.
+ */
+public class MyTestUtil {
+    public String getValue() {
+        return "Hello from Util class";
+    }
+}
